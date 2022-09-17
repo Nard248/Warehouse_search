ab05ce2be8dcf879f8cf0d97548cb73685752b98
wks-onex-44 <meloyann87@gmail.com>
2022-09-17 12:48:56 +0400

    Rectangle with image poping up

--- Label_reader.py
+++ Label_reader.py
@@ -33,11 +33,13 @@ def reader_ocr(image):
                 if 'ARM' in l:
                     return_dict['arm_code'] = l
                     image = cv2.rectangle(image, top_left, bot_right, (255, 0, 0), 2)
-                    image = cv2.putText(image, l, (int(top_left[0]) + 25, int(top_left[1])), font, 2, (255, 0, 0), 2, cv2.LINE_AA)
+                    image = cv2.putText(image, l, (int(top_left[0]) + 25, int(top_left[1])), font, 2, (255, 0, 0), 2,
+                                        cv2.LINE_AA)
         if 'AYRE' in text:
             return_dict['address'] = text
             image = cv2.rectangle(image, top_left, bot_right, (0, 255, 0), 2)
-            image = cv2.putText(image, text, (int(top_left[0]) + 25, int(top_left[1])), font, 2, (0, 255, 0), 2, cv2.LINE_AA)
+            image = cv2.putText(image, text, (int(top_left[0]) + 25, int(top_left[1])), font, 2, (0, 255, 0), 2,
+                                cv2.LINE_AA)
         decoded = decode(image)
         for i in range(len(decoded)):
             tracking_dict[i] = str(decoded[i].data.decode('utf-8'))
@@ -50,5 +52,20 @@ def reader_ocr(image):
     return return_dict
 
 
-img = resize_image(IMAGE_PATH)
-print(reader_ocr(img))
+def test():
+    import math
+    from PIL import Image, ImageDraw
+
+    w, h = 220, 190
+    shape = [(40, 40), (w - 10, h - 10)]
+
+    # creating new Image object
+    img = Image.new("RGB", (w, h))
+
+    # create rectangle image
+    img1 = ImageDraw.Draw(img)
+    img1.rectangle(shape, fill="#ffff33", outline="red")
+    img.show()
+
+
+test()
--- google_reader.py
+++ google_reader.py
@@ -2,8 +2,8 @@ import os
 import cv2
 from google.cloud import vision
 import io
-import io
-from PIL import Image
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\User\Desktop\Warehouse_search_vision\token.json'
 
@@ -39,19 +39,19 @@ def detect_text(path):
 def detect_text_my(path):
     client = vision.ImageAnnotatorClient()
     img = Image.open(path)
-    print(img.size)
     width, height = img.size
     new_width = 1050
     new_height = height * (new_width / width)
-    img.thumbnail((new_width, new_height))
-    print(img.size)
+    img.thumbnail((int(new_width), int(new_height)))
     buffer = io.BytesIO()
     img.save(buffer, "JPEG")
     content = buffer.getvalue()
     image = vision.Image(content=content)
-
     response = client.text_detection(image=image)
-    print(response.text_annotations[13].description)
+    print(response.text_annotations[0].bounding_poly.vertices)
+    img1 = ImageDraw.Draw(img)
+    img1.rectangle([(response.text_annotations[0].bounding_poly.vertices[0].x, response.text_annotations[0].bounding_poly.vertices[0].y), (response.text_annotations[0].bounding_poly.vertices[2].x, response.text_annotations[0].bounding_poly.vertices[2].y)], outline='red')
+    img.show()
     # texts = response.text_annotations
     #
     # for text in texts:
